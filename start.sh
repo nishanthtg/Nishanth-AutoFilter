64de234c50ca271cdaa55fbfc71a95373a6c8754
nishanthtg <109413626+nishanthtg@users.noreply.github.com>
2022-07-16 17:13:26 +0530

    try

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/nishanthtg/Nishanth-AutoFilter.git /Nishanth-AutoFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Nishanth-AutoFilter
 fi
-cd /EvaMaria
+cd /Nishanth-AutoFilter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py

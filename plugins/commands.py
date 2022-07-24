eda3de6fc9ac579f852a5e04911fdc08541d6cdf
nishanthtg <109413626+nishanthtg@users.noreply.github.com>
2022-07-24 22:53:16 +0530

    Done

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,7 +51,7 @@ async def start(client, message):
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_message(
+        await message.reply_photo(
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
@@ -90,7 +90,7 @@ async def start(client, message):
             InlineKeyboardButton('Movie Request', url='https://t.me/VideoMemesAdmin')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_message(
+        await message.reply_photo(
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'

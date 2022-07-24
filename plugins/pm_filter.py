7d7007090f2fa7750a62aecb73feea1973d9c0be
nishanthtg <109413626+nishanthtg@users.noreply.github.com>
2022-07-24 20:41:01 +0530

    Mm

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -641,7 +641,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=f"https://telegram.dog/{temp.U_NAME}?start={file.file_id}"
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
